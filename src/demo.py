fc56b715112354e89cf6c354cc9b63e6d60d0725
Damien Hodsall <damien.hodsall@gmail.com>
2023-10-19 09:33:38 -0700

    Added documentation in scripts

--- src/demo.py
+++ src/demo.py
@@ -3,21 +3,28 @@ import matplotlib.pyplot as plt
 from svg_handler import SVG_Handler
 from matplotlib.widgets import Button
 
+"""
+This script is a 3d demonstration for parametric equations.
+
+Just run it and click through the different views and move the view around.
+I set it up to show xt then yt and finaly xy. When looking at xy, mention that it's
+how parametric equations work.
+"""
+
+# read the image and make it into a list of points
 handler = SVG_Handler('share/BatlineArt.svg')
-
 t = np.linspace(0,1,5000)
-
 p = handler.get_point(t)
 
+# initialize graphing stuff
 fig, ax = plt.subplots()
 plot3d = plt.subplot(projection='3d')
 
+# actual plotting of data
 plot3d.plot(p.real,-p.imag,t)
-#plot3d.view_init(90,0,'x') # yt
-#plot3d.view_init(90,-90,'y') # xt
-#plot3d.view_init(90,-90,'z') # xy
 plot3d.set(xlabel='x', ylabel='y',zlabel='t')
 
+# these views are xt yt xy
 views = {
         0: (90, -90, 'y'),
         1: (90, 0, 'x'),
@@ -26,14 +33,23 @@ views = {
 currentview = 0
 
 # buttons for views
-nextax = fig.add_axes([0.8,0.025,0.1,0.04])
+nextax = fig.add_axes([0.9,0.025,0.1,0.04])
 nextbutton = Button(nextax, 'Next', hovercolor='0.95')
+
+resetax = fig.add_axes([0.8,0.025,0.1,0.04])
+resetbutton = Button(resetax, 'Reset', hovercolor='0.95')
+
 prevax = fig.add_axes([0.7,0.025,0.1,0.04])
 prevbutton = Button(prevax, 'Prev', hovercolor='0.95')
 
+# functions to switch/reset views
 def nextview(event):
     global currentview
     currentview = (currentview + 1) % 3
+    plot3d.view_init(*views[currentview])
+
+def resetview(event):
+    global currentview
     plot3d.view_init(*views[currentview])
 
 def prevview(event):
@@ -41,8 +57,9 @@ def prevview(event):
     currentview = (currentview - 1) % 3
     plot3d.view_init(*views[currentview])
 
+# uhhhh yeah. this kinda explains itself... read the python matplotlib widgets if you don't get it
 nextbutton.on_clicked(nextview)
+resetbutton.on_clicked(resetview)
 prevbutton.on_clicked(prevview)
 
-plot3d.view_init(*views[currentview])
 plt.show()
--- src/oscilloscope.py
+++ src/oscilloscope.py
@@ -3,6 +3,14 @@ import wavio
 import matplotlib.pyplot as plt
 from svg_handler import SVG_Handler
 
+"""
+This script makes an audio output from an svg. The left and right channels of the output
+are the x and y of the image. Pass these channels into an oscilloscope and put it into xy
+mode to show the image on the oscilloscope. plt is used for testing and a rough preview of
+the oscilloscope. demo.py can be paired with this script to explain the concept of
+paremetric equations with a 3d plot of the bat image.
+"""
+
 # this can be changed to alter the frequency of the output sound
 # it seems like ω α 1/plotOrder
 plotOrder = 800
@@ -14,17 +22,18 @@ images = {
         'pumpkin1':'share/JackOlanternLines.svg',
         'pumpkin2':'share/pumpkinmaybenotahunted.svg',
         'pumpkin3':'share/jackOlantern2.svg',
-        'ghost':'share/ghost-ish.svg',
+        'ghost':'share/halloween-ghost-svgrepo-com.svg',
         'lightning':'share/cloud-bolt-svgrepo-com.svg',
         'skeleton':'share/skeleton-svgrepo-com.svg',
-        'grave1':'share/grave-illustration-3-svgrepo-com.svg'
+        'grave1':'share/grave-illustration-3-svgrepo-com.svg',
+        'sad':'share/sad-face-svgrepo-com.svg'
         }
 
 # SVG_Handler.get_points takes all the paths of the svg and makes them into a function
 # with domain [0,1) (so use np.linspace(0,1))
 # takes a single float or a list/array
 # returns an array
-handler = SVG_Handler(images['grave1'])
+handler = SVG_Handler(images['bat'])
 p = handler.get_point(t)
 
 # signal must be <= 1 so divide by the largest magnitude to normalize (maybe not the most efficient but was quick to implement)
